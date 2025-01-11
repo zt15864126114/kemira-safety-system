fc70556cc5b9a6a6f67064b94c95c8adcdde595d
zhaoteng <1813831021@qq.com>
2025-01-12 02:30:58 +0800

    第二次

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,15 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
+  base: '/kemira-safety-system/', // 添加这行，路径应该是你的仓库名
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  build: {
+    outDir: 'dist', // 可选：指定构建输出目录
+    assetsDir: 'assets', // 可选：指定资源文件目录
   }
 }) 
